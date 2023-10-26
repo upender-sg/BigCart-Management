ec0159ea9975915799c7f2ad04b14b31fb321f04
Upender ShanaGonda <59762973+upender-sg@users.noreply.github.com>
2023-10-26 11:01:32 +0200

    Update apps.py

--- backend/usermanager/apps.py
+++ backend/usermanager/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UsermanagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usermanager'
+    name = 'usermanager'/'user_name'
